40a455bb934a9bb06a77d890d67c5a9f916e749e
Andrew McWilliams <andy@jahya.net>
2014-08-17 20:20:21 -0400

    Splits weighted probability examples away from fully random ones

--- of083/apps/experiments/1b. weightedProbabilities/src/ofApp.cpp
+++ of083/apps/experiments/1b. weightedProbabilities/src/ofApp.cpp
@@ -26,56 +26,84 @@ void ofApp::draw(){
 	ofDisableAlphaBlending();
 }
 
-//---------- Experiment 1: Objects appear randomly, overlapping
-ofImage img1;
+//---------- Experiment 1: Two images with slim chances of appearing
+map<int, ofImage> imgs3;
+ofImage img1, img2, noImage;
 
-void ofApp::setupExp1(){
+void ofApp::setupExp1() {
 	img1.loadImage("1.png");
+    img2.loadImage("2.png");
+    noImage.allocate(1, 1, OF_IMAGE_COLOR);
+    imgs3[0]  = img1;
+    imgs3[1]  = img2;
+    imgs3[2]  = img2;
+    imgs3[3]  = noImage;
+    imgs3[4]  = noImage;
+    imgs3[5]  = noImage;
+    imgs3[6]  = noImage;
+    imgs3[7]  = noImage;
+    imgs3[8]  = noImage;
+    imgs3[9]  = noImage;
+    imgs3[10] = noImage;
 }
 
 void ofApp::drawExp1(){
-	for(int i = -100; i < ofGetWidth(); i += 100) {
-        for(int j = -100; j < ofGetHeight(); j += 100) {
-            if ((int)ofRandom(2) == 0) {
-                img1.draw(i, j);
-            }
+	for(int i = -100; i < ofGetWidth(); i += 150) {
+        for(int j = -100; j < ofGetHeight(); j += 150) {
+            int index = (int)ofRandom(imgs3.size());
+            imgs3[index].draw(i, j);
         }
     }
+    ofSleepMillis(200);
 }
 
-//---------- Experiment 2: Objects appear randomly, non-overlapping
-int w, h;
+//---------- Experiment 2: Five images, with better chances of appearing
+map<int, ofImage> imgs4;
+ofImage img3, img4, img5;
 
 void ofApp::setupExp2() {
-    w = img1.getWidth(); h = img1.getHeight();
+    img3.loadImage("3.png");
+    img4.loadImage("4.png");
+    img5.loadImage("5.png");
+    imgs4[0]  = img1;
+    imgs4[1]  = img2;
+    imgs4[2]  = img3;
+    imgs4[3]  = img4;
+    imgs4[4]  = img5;
+    imgs4[5]  = img1;
+    imgs4[6]  = noImage;
+    imgs4[7]  = noImage;
+    imgs4[8]  = noImage;
+    imgs4[9]  = noImage;
+    imgs4[10] = noImage;
 }
 
 void ofApp::drawExp2(){
-    for(int i = -w; i < ofGetWidth() + w; i += w) {
-        for(int j = -h; j < ofGetHeight() + h; j += h) {
-            if ((int)ofRandom(2) == 0) {
-                img1.draw(i, j);
-            }
+	for(int i = -100; i < ofGetWidth(); i += 150) {
+        for(int j = -100; j < ofGetHeight(); j += 150) {
+            int index = (int)ofRandom(imgs4.size());
+            imgs4[index].draw(i, j);
         }
     }
+    ofSleepMillis(200);
 }
 
 //---------- Experiment 3:
 void ofApp::setupExp3() {
-    
+
 }
 
 void ofApp::drawExp3(){
-    
+
 }
 
 //---------- Experiment 4:
 void ofApp::setupExp4() {
-    
+
 }
 
 void ofApp::drawExp4(){
-    
+
 }
 
 //--------------------------------------------------------------
